284b3401aef21c96d55d610f1d05caf01e639b89
Laurel Perkins <lmperkins20@gmail.com>
2023-03-03 10:36:37 -0800

    ToDo: 1 complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import $ from "jquery";
-import "./App.scss";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import About from "./components/About";
-import Home from "./components/Home";
+import "../App.scss";
+import Header from "./Header";
+import Footer from "./Footer";
+import About from "./About";
+import Home from "./Home";
 
 class App extends Component {
 
